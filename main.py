3ac99b621091dcb2ee12ed01007082839c897c80
larrenzcarino <lecarino@uci.edu>
2024-04-01 11:26:05 -0700

    Debug and changed frontend

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
-from flask import Flask, render_template, request, redirect, url_for
-from flask_bootstrap import Bootstrap
-# from datetime import datetime
+from flask import Flask, render_template, request
+from flask_bootstrap import Bootstrap5
 from PIL import Image
 import numpy as np
 import os
 from colorthief import ColorThief
-# from memory_profiler import profile
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get("FLASK_KEY")
+Bootstrap5(app)
 
 @app.route('/', methods=['GET', 'POST'])
-# @profile
 def home():
-    img_path = None 
+    img_path = None
     palette = None
     if request.method == 'POST':
-        #check if a file was provided
         if 'file' in request.files:
             file = request.files['file']
             try:
@@ -27,13 +24,29 @@ def home():
                 image = Image.open(file)
                 img_array = np.array(image)
 
-                color_thief = ColorThief(file)
-                palette = [(index + 1, color) for index, color in enumerate(color_thief.get_palette(color_count= color_count))]
+                # Convert RGBA image to RGB
+                rgb_image = Image.fromarray(img_array).convert('RGB')
 
-                # Save the image to a file in the static folder
+                # Save the RGB image to a temporary file
+                temp_img_path = 'temp_image.jpg'
+                rgb_image.save(temp_img_path)
+
+                color_thief = ColorThief(temp_img_path)
+                palette = [(index + 1, color) for index, color in enumerate(color_thief.get_palette(color_count=color_count))]
+
+                # Define the directory path
+                directory = os.path.join('static', 'assets', 'img')
+
+                # Create the directory if it does not exist
+                if not os.path.exists(directory):
+                    os.makedirs(directory)
+
+                # Update the file path to include the directory
                 img_filename = 'uploaded_image.jpg'
-                img_path = os.path.join('static', img_filename)
-                Image.fromarray(img_array).save(img_path)
+                img_path = os.path.join(directory, img_filename)
+
+                # Move the temporary image to the final location
+                os.rename(temp_img_path, img_path)
 
     colors = palette if palette is not None else []
     return render_template('index.html', img_path=img_path, colors=colors)
